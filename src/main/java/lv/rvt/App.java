7fff1a50880012cc4227b76a49f8bf6750e90c54
Marat Razzivin <marat061206@gmail.com>
2024-10-01 14:00:06 +0300

    devisibleByThreeInRange

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,38 +7,15 @@ import javax.swing.plaf.TreeUI;
 public class App {
     public static void main( String[] args ) {
         
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Give numbers:");
-        
-        int sum = 0;
-        int numCount = 0;
-        int even = 0;
-        int odd = 0;
+        devisibleByThreeInRange(2, 10);
 
-        while (true) {
-            int number = Integer.valueOf(scanner.nextLine());
+    }
 
-            if (number == -1) {
-                break;
+    public static void devisibleByThreeInRange(int first, int second) {
+        for (int i = first; i < (second + 1); i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
             }
-
-            if (number % 2 == 0) {
-                even ++;
-            }
-            else {
-                odd++;
-            }
-
-            sum += number;
-            numCount ++;
         }
-
-        System.out.println("Thx! Bye!");
-        System.out.println("Sum: " + sum);
-        System.out.println("Numbers: " + numCount);
-        System.out.println("Average: " + ((double)sum / numCount));
-        System.out.println("Even: " + even);
-        System.out.println("Odd: " + odd);
     }
 }
