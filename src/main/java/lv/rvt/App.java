ec3081fefbdb0240da4314893d5fdd98ed5da93e
Marat Razzivin <marat061206@gmail.com>
2024-10-08 09:56:30 +0300

    Star sign

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,16 +6,42 @@ import javax.swing.plaf.TreeUI;
 
 public class App {
     public static void main( String[] args ) {
-        
-        devisibleByThreeInRange(2, 10);
+        printStars(5);
+        printStars(3);
+        printStars(9);
+        System.out.println();
 
+        printSquare(4);
+        System.out.println();
+
+        printRectangle(17, 3);
+        System.out.println();
+
+        printTriangle(4);
     }
 
-    public static void devisibleByThreeInRange(int first, int second) {
-        for (int i = first; i < (second + 1); i++) {
-            if (i % 3 == 0) {
-                System.out.println(i);
-            }
+    public static void printStars(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    public static void printSquare(int size) {
+        for (int i = 0; i < size; i++) {
+            printStars(size);
+        }
+    }
+
+    public static  void printRectangle(int width, int height) {
+        for (int i = 0; i < height; i++) {
+            printStars(width);
+        }
+    }
+
+    public static void printTriangle(int size) {
+        for (int i = 0; i < size; i++) {
+            printStars(i+1);
         }
     }
 }
