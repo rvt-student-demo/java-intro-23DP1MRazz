86125fd46c303054a1281d5ab6094add459f3986
Marat Razzivin <marat061206@gmail.com>
2024-10-08 10:52:49 +0300

    Christmas tree

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -18,6 +18,12 @@ public class App {
         System.out.println();
 
         printTriangle(4);
+        System.out.println();
+
+        printRightTriangle(4);
+        System.out.println();
+
+        christmasTree(10);
     }
 
     public static void printStars(int number) {
@@ -42,6 +48,30 @@ public class App {
     public static void printTriangle(int size) {
         for (int i = 0; i < size; i++) {
             printStars(i+1);
+        }
+    }
+
+    public static void printSpaces(int number) {
+        for (int i = number - 1; i > 0; i--) {
+            System.out.print(" ");
+        }
+    }
+
+    public static void printRightTriangle(int size) {
+        for (int i = 0; i < size; i++) {
+            printSpaces(size - i);
+            printStars(i + 1);
+        }
+    }
+
+    public static void christmasTree(int height) {
+        for (int i = 0; i < height; i++) {
+            printSpaces(height - i);
+            printStars((i + 1) + i);
+        }
+        for (int a = 0; a < 2; a++) {
+            printSpaces(height - 1);
+            printStars(3);
         }
     }
 }
