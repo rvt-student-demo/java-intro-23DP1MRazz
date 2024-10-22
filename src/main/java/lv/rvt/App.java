072eb6e51b6c9858db3b0076cc7c9c2c9150c5b4
Marat Razzivin <marat061206@gmail.com>
2024-10-22 09:37:46 +0300

    Only these numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,19 +7,36 @@ public class App {
     public static void main( String[] args ) {
         Scanner scanner = new Scanner(System.in);
 
-        ArrayList<Integer> intList = new ArrayList<>();
+        ArrayList<Integer> numbers = new ArrayList<>();
 
-        int sum = 0;
         while (true) {
             int num = scanner.nextInt();
-            if (num == 0) {
+            if (num == -1) {
                 break;
-            }
-            else {
-                intList.add(num);
-                sum += num;
+            } else {
+                numbers.add(num);
             }
         }
-        System.out.println(sum);
+        System.out.println("From where? ");
+        int start = scanner.nextInt();
+
+        System.out.println("To where? ");
+        int stop = scanner.nextInt();
+        while (true) {
+
+            if (start < 0 || stop > (numbers.size() - 1)) {
+                System.out.println("Incorrect index!");
+
+                System.out.println("From where? ");
+                start = scanner.nextInt();
+                System.out.println("To where? ");
+                stop = scanner.nextInt();
+            }
+            break;
+        }
+
+        for (int i = start; i < (stop + 1); i++) {
+            System.out.println(numbers.get(i));
+        }
     }
 }
