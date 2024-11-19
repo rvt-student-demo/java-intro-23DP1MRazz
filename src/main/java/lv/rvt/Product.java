852e201d9b90ce7dba5fae85a7a12fa1669cbbc6
Marat Razzivin <marat061206@gmail.com>
2024-11-19 10:43:17 +0200

    Constructor overload

--- src/main/java/lv/rvt/Product.java
+++ src/main/java/lv/rvt/Product.java
@@ -2,16 +2,28 @@ package lv.rvt;
 
 public class Product {
     private String name;
-    private double price;
-    private int quantity;
+    private String location;
+    private int weight;
 
-    public Product(String initialName, double initialPrice, int initialQuantity) {
-        this.name = initialName;
-        this.price = initialPrice;
-        this.quantity = initialQuantity;
+    public Product(String name, String location, int weight) {
+        this.name = name;
+        this.location = location;
+        this.weight = weight;
     }
 
-    public void printProduct() {
-        System.out.println(this.name + ", price " + this.price + ", " + this.quantity + " pcs");
+    public Product(String name) {
+        this(name, "shelf", 0);
+    }
+    
+    public Product(String name, String location) {
+        this(name, location, 0);
+    }
+
+    public Product(String name, int weight) {
+        this(name, "shelf", weight);
+    }
+
+    public String toString() {
+        return name + " (" + weight + "kg) can be found from the " + location;
     }
 }
