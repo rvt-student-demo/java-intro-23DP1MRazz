8f00109aac6228b7c742df35f121773e7136ce13
Marat Razzivin <marat061206@gmail.com>
2024-12-03 10:18:37 +0200

    PersonManager (part 1)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,9 +1,14 @@
 package lv.rvt;
 
+import java.io.BufferedReader;
 import java.util.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        ArrayList<Person> persons = PersonManager.getPersonList();
+
+        for (Person person : persons) {
+            System.out.println(person);
+        }
     }
 }
---
+++ src/main/java/lv/rvt/PersonManager.java
@@ -0,0 +1,23 @@
+package lv.rvt;
+
+import java.io.BufferedReader;
+import java.util.ArrayList;
+
+public class PersonManager {
+    public static ArrayList<Person> getPersonList() throws Exception {
+        ArrayList<Person> persons = new ArrayList<>();
+
+        BufferedReader reader = Helper.getReader("person.csv");
+        reader.readLine();
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] parts = line.split(", ");
+
+            Person person1 = new Person(parts[0], Integer.valueOf(parts[1]), Integer.valueOf(parts[2]), Integer.valueOf(parts[3]));
+            persons.add(person1);
+        }
+
+        return persons;
+    }
+}
